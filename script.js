60e9c0a2bb7a0edfba0be29792dd11ed85638ebd
Evere77 <evere777@yahoo.com>
2023-03-21 15:52:50 -0700

    Dojo Pizzeria

---
+++ script.js
@@ -0,0 +1,9 @@
+function pizzaOven(pizza) {
+  var pizza = {};
+    pizza.crustType = deep dish;
+    pizza.sauceType =  traditional;
+    pizza.cheese = mozzarella;
+    pizza.toppings = ['pepperoni','sausage'];
+    return pizza;
+}
+
