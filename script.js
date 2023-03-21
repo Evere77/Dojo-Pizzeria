7cd89e2d6fbc911dfa881fd405ac08186e123f85
Evere77 <evere777@yahoo.com>
2023-03-21 16:03:26 -0700

    updating

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-function pizzaOven(pizza) {
+function pizzaOven(crustType, sauceType, cheese, toppings) {
   var pizza = {};
-    pizza.crustType = deep dish;
-    pizza.sauceType =  traditional;
-    pizza.cheese = mozzarella;
-    pizza.toppings = ['pepperoni','sausage'];
+    pizza.crustType = crustType;
+    pizza.sauceType =  sauceType;
+    pizza.cheese = cheese;
+    pizza.toppings = toppings;
     return pizza;
 }
 
+var pizza1 = pizzaOven('deep dish', 'traditional', ['mozzarella'], ['pepperoni', 'sausage']);
+console.log(pizza1);
+
+var pizza2 = pizzaOven('hand tossed', 'marinara', ['mozzarella', 'feta'], ['mushrooms', 'olives', 'onions']);
+console.log(pizza2);
+
+var pizza3 = pizzaOven('deep dish','marinara', ['mozzarella'], ['pepperoni', 'green pepper', 'onions']);
+console.log(pizza3);
+
+var pizza4 = pizzaOven('deep dish', 'marinara', ['mozzarella'], ['pepperoni', 'sausage', 'bacon','green pepper', 'onions']);
+console.log(pizza4);
